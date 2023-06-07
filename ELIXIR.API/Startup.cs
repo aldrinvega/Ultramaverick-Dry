8fcc83f22128105ccbdecdb043f1299b862b7db8
aldrin0408av <aldrinmundovega@gmail.com>
2023-06-07 19:23:58 +0800

    Adjust Connection

--- ELIXIR.API/Startup.cs
+++ ELIXIR.API/Startup.cs
@@ -70,7 +70,7 @@ namespace ELIXIR.API
             services.AddScoped(typeof(IUserService), typeof(UserService));
       
             services.AddDbContext<StoreContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("ProdConnection")));
 
             services.Configure<ApiBehaviorOptions>(options =>
             {
