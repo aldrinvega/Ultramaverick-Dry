f416a96ffa1c9c31a66cb17146494c4ffe476d73
Aldrin Vega <aldrinmundovega@gmail.com>
2024-04-04 13:45:54 +0800

    add: pagination for all reports execpt Consolidated Reports kasi mahirap

--- ELIXIR.API/Controllers/REPORT_CONTROLLER/ReportController.cs
+++ ELIXIR.API/Controllers/REPORT_CONTROLLER/ReportController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using ELIXIR.DATA.DATA_ACCESS_LAYER.EXTENSIONS;
 using ELIXIR.DATA.DATA_ACCESS_LAYER.HELPERS;
+using System.Drawing;
 
 namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
 {
@@ -25,6 +26,37 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
         }
 
         [HttpGet]
+        [Route("QcReceivingReportPagination")]
+        public async Task<IActionResult> QcReceivingReportPagination([FromQuery] string dateFrom,
+           [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        {
+            var qCReports = await _unitOfWork.Report
+                .QcRecevingReportPagination(dateFrom, dateTo, userParams);
+
+            Response.AddPaginationHeader(
+                qCReports.CurrentPage,
+                qCReports.PageSize,
+                qCReports.TotalCount,
+                qCReports.TotalPages,
+                qCReports.HasPreviousPage,
+                qCReports.HasNextPage
+                );
+
+            var result = new
+            {
+                qCReports,
+                qCReports.CurrentPage,
+                qCReports.PageSize,
+                qCReports.TotalCount,
+                qCReports.TotalPages,
+                qCReports.HasPreviousPage,
+                qCReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
         [Route("WarehouseReceivingReport")]
         public async Task<IActionResult> WarehouseReceivingReport([FromQuery] string DateFrom,
             [FromQuery] string DateTo)
@@ -33,6 +65,38 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
 
             return Ok(orders);
         }
+
+        [HttpGet]
+        [Route("WarehouseReceivingReportPagination")]
+        public async Task<IActionResult> WarehouseReceivingReportPagination([FromQuery] string dateFrom,
+          [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        {
+            var warehouseReports = await _unitOfWork.Report
+                .WarehouseReceivingReportPagination(dateFrom, dateTo, userParams);
+
+            Response.AddPaginationHeader(
+                warehouseReports.CurrentPage,
+                warehouseReports.PageSize,
+                warehouseReports.TotalCount,
+                warehouseReports.TotalPages,
+                warehouseReports.HasPreviousPage,
+                warehouseReports.HasNextPage
+                );
+
+            var result = new
+            {
+                warehouseReports,
+                warehouseReports.CurrentPage,
+                warehouseReports.PageSize,
+                warehouseReports.TotalCount,
+                warehouseReports.TotalPages,
+                warehouseReports.HasPreviousPage,
+                warehouseReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
 
         [HttpGet]
         [Route("TransformationHistoryReport")]
@@ -45,12 +109,74 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
         }
 
         [HttpGet]
+        [Route("TransformationHistoryReportPagination")]
+        public async Task<IActionResult> TransformationHistoryReportPagination([FromQuery] string dateFrom,
+          [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        {
+            var transformationReports = await _unitOfWork.Report
+                .TransformationReportPagination(dateFrom, dateTo, userParams);
+
+            Response.AddPaginationHeader(
+                transformationReports.CurrentPage,
+                transformationReports.PageSize,
+                transformationReports.TotalCount,
+                transformationReports.TotalPages,
+                transformationReports.HasPreviousPage,
+                transformationReports.HasNextPage
+                );
+
+            var result = new
+            {
+                transformationReports,
+                transformationReports.CurrentPage,
+                transformationReports.PageSize,
+                transformationReports.TotalCount,
+                transformationReports.TotalPages,
+                transformationReports.HasPreviousPage,
+                transformationReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
         [Route("MoveOrderHistory")]
         public async Task<IActionResult> MoveOrderHistory([FromQuery] string DateFrom, [FromQuery] string DateTo)
         {
             var orders = await _unitOfWork.Report.MoveOrderReport(DateFrom, DateTo);
 
             return Ok(orders);
+        }
+
+        [HttpGet]
+        [Route("MoveOrderHistoryPagination")]
+        public async Task<IActionResult> MoveOrderHistoryPagination([FromQuery] string dateFrom,
+        [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        {
+            var moveOrderReports = await _unitOfWork.Report
+                .MoveOrderReportPagination(dateFrom, dateTo, userParams);
+
+            Response.AddPaginationHeader(
+                moveOrderReports.CurrentPage,
+                moveOrderReports.PageSize,
+                moveOrderReports.TotalCount,
+                moveOrderReports.TotalPages,
+                moveOrderReports.HasPreviousPage,
+                moveOrderReports.HasNextPage
+                );
+
+            var result = new
+            {
+                moveOrderReports,
+                moveOrderReports.CurrentPage,
+                moveOrderReports.PageSize,
+                moveOrderReports.TotalCount,
+                moveOrderReports.TotalPages,
+                moveOrderReports.HasPreviousPage,
+                moveOrderReports.HasNextPage
+            };
+
+            return Ok(result);
         }
 
         [HttpGet]
@@ -64,6 +190,37 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
         }
 
         [HttpGet]
+        [Route("MiscellaneousReceiptReportPagination")]
+        public async Task<IActionResult> MiscellaneousReceiptReportPagination([FromQuery] string dateFrom,
+  [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        {
+            var miscellaneousReceiptReports = await _unitOfWork.Report
+                .MReceiptReportPagination(dateFrom, dateTo, userParams);
+
+            Response.AddPaginationHeader(
+                miscellaneousReceiptReports.CurrentPage,
+                miscellaneousReceiptReports.PageSize,
+                miscellaneousReceiptReports.TotalCount,
+                miscellaneousReceiptReports.TotalPages,
+                miscellaneousReceiptReports.HasPreviousPage,
+                miscellaneousReceiptReports.HasNextPage
+                );
+
+            var result = new
+            {
+                miscellaneousReceiptReports,
+                miscellaneousReceiptReports.CurrentPage,
+                miscellaneousReceiptReports.PageSize,
+                miscellaneousReceiptReports.TotalCount,
+                miscellaneousReceiptReports.TotalPages,
+                miscellaneousReceiptReports.HasPreviousPage,
+                miscellaneousReceiptReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
         [Route("MiscellaneousIssueReport")]
         public async Task<IActionResult> MiscellaneousIssueReport([FromQuery] string DateFrom,
             [FromQuery] string DateTo)
@@ -74,12 +231,72 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
         }
 
         [HttpGet]
+        [Route("MiscellaneousIssueReportPagination")]
+        public async Task<IActionResult> MiscellaneousIssueReportPagination([FromQuery] string dateFrom,
+  [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        {
+            var miscellaneousIssueReports = await _unitOfWork.Report
+                .MIssueReportPagination(dateFrom, dateTo, userParams);
+
+            Response.AddPaginationHeader(
+                miscellaneousIssueReports.CurrentPage,
+                miscellaneousIssueReports.PageSize,
+                miscellaneousIssueReports.TotalCount,
+                miscellaneousIssueReports.TotalPages,
+                miscellaneousIssueReports.HasPreviousPage,
+                miscellaneousIssueReports.HasNextPage
+                );
+
+            var result = new
+            {
+                miscellaneousIssueReports,
+                miscellaneousIssueReports.CurrentPage,
+                miscellaneousIssueReports.PageSize,
+                miscellaneousIssueReports.TotalCount,
+                miscellaneousIssueReports.TotalPages,
+                miscellaneousIssueReports.HasPreviousPage,
+                miscellaneousIssueReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
         [Route("NearlyExpireItemsReport")]
         public async Task<IActionResult> NearlyExpireItemsReport([FromQuery] int expirydays)
         {
             var expiry = await _unitOfWork.Report.NearlyExpireItemsReport(expirydays);
 
             return Ok(expiry);
+        }
+        [HttpGet]
+        [Route("NearlyExpireItemsReportPagination")]
+        public async Task<IActionResult> NearlyExpireItemsReportPagination([FromQuery] int expirydays, [FromQuery] UserParams userParams)
+        {
+            var nealyExpiryItemsReports = await _unitOfWork.Report
+                .NearlyExpireItemsReportPagination(expirydays, userParams);
+
+            Response.AddPaginationHeader(
+                nealyExpiryItemsReports.CurrentPage,
+                nealyExpiryItemsReports.PageSize,
+                nealyExpiryItemsReports.TotalCount,
+                nealyExpiryItemsReports.TotalPages,
+                nealyExpiryItemsReports.HasPreviousPage,
+                nealyExpiryItemsReports.HasNextPage
+                );
+
+            var result = new
+            {
+                nealyExpiryItemsReports,
+                nealyExpiryItemsReports.CurrentPage,
+                nealyExpiryItemsReports.PageSize,
+                nealyExpiryItemsReports.TotalCount,
+                nealyExpiryItemsReports.TotalPages,
+                nealyExpiryItemsReports.HasPreviousPage,
+                nealyExpiryItemsReports.HasNextPage
+            };
+
+            return Ok(result);
         }
 
         [HttpGet]
@@ -93,6 +310,36 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
         }
 
         [HttpGet]
+        [Route("TransactedMoveOrderReportPagination")]
+        public async Task<IActionResult> TransactedMoveOrderReportPagination([FromQuery] string DateFrom, [FromQuery] string DateTo, [FromQuery] UserParams userParams)
+        {
+            var transactedMoveOrderReports = await _unitOfWork.Report
+                .TransactedMoveOrderReportPagination(DateFrom, DateTo, userParams);
+
+            Response.AddPaginationHeader(
+                transactedMoveOrderReports.CurrentPage,
+                transactedMoveOrderReports.PageSize,
+                transactedMoveOrderReports.TotalCount,
+                transactedMoveOrderReports.TotalPages,
+                transactedMoveOrderReports.HasPreviousPage,
+                transactedMoveOrderReports.HasNextPage
+                );
+
+            var result = new
+            {
+                transactedMoveOrderReports,
+                transactedMoveOrderReports.CurrentPage,
+                transactedMoveOrderReports.PageSize,
+                transactedMoveOrderReports.TotalCount,
+                transactedMoveOrderReports.TotalPages,
+                transactedMoveOrderReports.HasPreviousPage,
+                transactedMoveOrderReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
         [Route("CancelledOrderReport")]
         public async Task<IActionResult> CancelledOrderReport([FromQuery] string DateFrom, [FromQuery] string DateTo)
         {
@@ -102,12 +349,72 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
         }
 
         [HttpGet]
+        [Route("CancelledOrderReportsPagination")]
+        public async Task<IActionResult> CancelledOrderReportsPagination([FromQuery] string DateFrom, [FromQuery] string DateTo, [FromQuery] UserParams userParams)
+        {
+            var cancelledOrderReports = await _unitOfWork.Report
+                .CancelledOrderedReportsPagination(DateFrom, DateTo, userParams);
+
+            Response.AddPaginationHeader(
+                cancelledOrderReports.CurrentPage,
+                cancelledOrderReports.PageSize,
+                cancelledOrderReports.TotalCount,
+                cancelledOrderReports.TotalPages,
+                cancelledOrderReports.HasPreviousPage,
+                cancelledOrderReports.HasNextPage
+                );
+
+            var result = new
+            {
+                cancelledOrderReports,
+                cancelledOrderReports.CurrentPage,
+                cancelledOrderReports.PageSize,
+                cancelledOrderReports.TotalCount,
+                cancelledOrderReports.TotalPages,
+                cancelledOrderReports.HasPreviousPage,
+                cancelledOrderReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
         [Route("InventoryMovementReport")]
         public async Task<IActionResult> InventoryMovementReport([FromQuery] string DateFrom, [FromQuery] string DateTo,
             [FromQuery] string PlusOne)
         {
             var cancel = await _unitOfWork.Report.InventoryMovementReport(DateFrom, DateTo, PlusOne);
             return Ok(cancel);
+        }
+
+        [HttpGet]
+        [Route("InventoryMovementReportPagination")]
+        public async Task<IActionResult> InventoryMovementReportPagination([FromQuery] string DateFrom, [FromQuery] string DateTo, [FromQuery] UserParams userParams)
+        {
+            var inventoryMovementReports = await _unitOfWork.Report
+                .ApprovedMoveOrderReportPagination(DateFrom, DateTo, userParams);
+
+            Response.AddPaginationHeader(
+                inventoryMovementReports.CurrentPage,
+                inventoryMovementReports.PageSize,
+                inventoryMovementReports.TotalCount,
+                inventoryMovementReports.TotalPages,
+                inventoryMovementReports.HasPreviousPage,
+                inventoryMovementReports.HasNextPage
+                );
+
+            var result = new
+            {
+                inventoryMovementReports,
+                inventoryMovementReports.CurrentPage,
+                inventoryMovementReports.PageSize,
+                inventoryMovementReports.TotalCount,
+                inventoryMovementReports.TotalPages,
+                inventoryMovementReports.HasPreviousPage,
+                inventoryMovementReports.HasNextPage
+            };
+
+            return Ok(result);
         }
 
         [HttpGet]
@@ -125,7 +432,39 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
             var approvedMoveOrderReports = await _unitOfWork.Report.ApprovedMoveOrderReport(dateFrom, dateTo);
             return Ok(approvedMoveOrderReports);
         }
-        
+
+
+
+        [HttpGet]
+        [Route("ApprovedMoveOrdersReportsPagination")]
+        public async Task<IActionResult> ApprovedMoveOrdersReportsPagination([FromQuery] string DateFrom, [FromQuery] string DateTo, [FromQuery] UserParams userParams)
+        {
+            var approvedMoveOrderReports = await _unitOfWork.Report
+                .ApprovedMoveOrderReportPagination(DateFrom, DateTo, userParams);
+
+            Response.AddPaginationHeader(
+                approvedMoveOrderReports.CurrentPage,
+                approvedMoveOrderReports.PageSize,
+                approvedMoveOrderReports.TotalCount,
+                approvedMoveOrderReports.TotalPages,
+                approvedMoveOrderReports.HasPreviousPage,
+                approvedMoveOrderReports.HasNextPage
+                );
+
+            var result = new
+            {
+                approvedMoveOrderReports,
+                approvedMoveOrderReports.CurrentPage,
+                approvedMoveOrderReports.PageSize,
+                approvedMoveOrderReports.TotalCount,
+                approvedMoveOrderReports.TotalPages,
+                approvedMoveOrderReports.HasPreviousPage,
+                approvedMoveOrderReports.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
         [HttpGet]
         [Route("OrderVsServeReports")]
         public async Task<IActionResult> OrderVsServeReports([FromQuery] string dateFrom,
@@ -166,5 +505,65 @@ namespace ELIXIR.API.Controllers.REPORT_CONTROLLER
 
             return Ok(result);
         }
+
+        [HttpGet("ItemsWithBbdPagination")]
+        public async Task<IActionResult> ItemsWithBbdPagination([FromQuery] UserParams userParams)
+        {
+            var itemsWithBbd = await _unitOfWork.Report
+                .ItemswithBBDDTOsPagination(userParams);
+
+            Response.AddPaginationHeader(
+                itemsWithBbd.CurrentPage,
+                itemsWithBbd.PageSize,
+                itemsWithBbd.TotalCount,
+                itemsWithBbd.TotalPages,
+                itemsWithBbd.HasPreviousPage,
+                itemsWithBbd.HasNextPage
+                );
+
+            var result = new
+            {
+                itemsWithBbd,
+                itemsWithBbd.CurrentPage,
+                itemsWithBbd.PageSize,
+                itemsWithBbd.TotalCount,
+                itemsWithBbd.TotalPages,
+                itemsWithBbd.HasPreviousPage,
+                itemsWithBbd.HasNextPage
+            };
+
+            return Ok(result);
+        }
+
+        //[HttpGet]
+        //[Route("ConsolidatedReportsPagination")]
+        //public async Task<IActionResult> ConsolidatedReportsPagination([FromQuery] string dateFrom,
+        //    [FromQuery] string dateTo, [FromQuery] UserParams userParams)
+        //{
+        //    var consolidatedReports = await _unitOfWork.Report
+        //        .ConsolidatedReportPagination(dateFrom, dateTo, userParams);
+
+        //    Response.AddPaginationHeader(
+        //        consolidatedReports.CurrentPage,
+        //        consolidatedReports.PageSize,
+        //        consolidatedReports.TotalCount,
+        //        consolidatedReports.TotalPages,
+        //        consolidatedReports.HasPreviousPage,
+        //        consolidatedReports.HasNextPage
+        //        );
+
+        //    var result = new
+        //    {
+        //        consolidatedReports,
+        //        consolidatedReports.CurrentPage,
+        //        consolidatedReports.PageSize,
+        //        consolidatedReports.TotalCount,
+        //        consolidatedReports.TotalPages,
+        //        consolidatedReports.HasPreviousPage,
+        //        consolidatedReports.HasNextPage
+        //    };
+
+        //    return Ok(result);
+        //}
     }
 }
--- ELIXIR.DATA/CORE/INTERFACES/REPORT_INTERFACE/IReportRepository.cs
+++ ELIXIR.DATA/CORE/INTERFACES/REPORT_INTERFACE/IReportRepository.cs
@@ -29,5 +29,21 @@ namespace ELIXIR.DATA.CORE.INTERFACES.REPORT_INTERFACE
         Task<PagedList<OrderVsServeReportsDTO>> OrderVsServeReportsPagination(string dateFrom, string dateTo,
             UserParams userParams);
         Task<IReadOnlyList<ItemswithBBDDTO>> ItemswithBBDReport();
+        //Task<PagedList<ConsolidatedReport>> ConsolidatedReportPagination(string dateFrom, string dateTo, UserParams userParams);
+        Task<PagedList<ItemswithBBDDTO>> ItemswithBBDDTOsPagination(UserParams userParams);
+
+        Task<PagedList<QCReport>> QcRecevingReportPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<WarehouseReport>> WarehouseReceivingReportPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<TransformationReport>> TransformationReportPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<MoveOrderReport>> MoveOrderReportPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<MiscellaneousReceiptReport>> MReceiptReportPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<MiscellaneousIssueReport>> MIssueReportPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<WarehouseReport>> NearlyExpireItemsReportPagination(int expirydays, UserParams userParams);
+        Task<PagedList<MoveOrderReport>> TransactedMoveOrderReportPagination(string dateFrom, string dateTo, UserParams userParams);
+        Task<PagedList<CancelledOrderReport>> CancelledOrderedReportsPagination(string DateFrom, string DateTo, UserParams userParams);
+        Task<PagedList<InventoryMovementReport>> InventoryMovementReportPagination(string DateFrom,
+        string DateTo, string PlusOne, UserParams userParams);
+        Task<PagedList<MoveOrderReport>> ApprovedMoveOrderReportPagination(string dateFrom, string dateTo, UserParams userParams);
+ 
     }
 }
