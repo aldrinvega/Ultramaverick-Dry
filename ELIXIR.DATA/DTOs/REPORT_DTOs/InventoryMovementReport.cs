6c1b2079adc155da5bbe4d530cd868859819541b
Aldrin Vega <aldrinmundovega@gmail.com>
2024-04-04 16:11:32 +0800

    fix: inventory movement report

--- ELIXIR.DATA/DTOs/REPORT_DTOs/InventoryMovementReport.cs
+++ ELIXIR.DATA/DTOs/REPORT_DTOs/InventoryMovementReport.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ELIXIR.DATA.DTOs.REPORT_DTOs
+﻿namespace ELIXIR.DATA.DTOs.REPORT_DTOs
 {
     public class InventoryMovementReport
     {
